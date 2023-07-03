a3698919f2e3ba178f8084bc26f6d0d5caff4b16
David Frese <david.frese@active-group.de>
2023-07-03 18:25:06 +0200

    Add group-name for project.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject active-data "0.1.0-SNAPSHOT"
+(defproject de.active-group/active-data "0.1.0-SNAPSHOT"
   :description "Active Data: Support data modelling in Clojure"
   :url "https://github.com/active-group/active-data"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
