37bbebeb4aee2379c185acedc2cccb842f4e6b5a
Mike Sperber <sperber@deinprogramm.de>
2023-07-03 17:49:29 +0200

    Empty project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Active Data
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject active-data "0.1.0-SNAPSHOT"
+  :description "Active Data: Support data modelling in Clojure"
+  :url "https://github.com/active-group/active-data"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]])
