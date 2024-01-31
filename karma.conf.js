856793939c7ae72a869b91d030495999a3ecf37c
David Frese <david.frese@active-group.de>
2024-01-31 18:18:32 +0100

    Switch to karma-jsdom

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,10 @@
 module.exports = function (config) {
     config.set({
-        browsers: ['ChromeHeadless'],
+        browsers: ['jsdom'],
         basePath: 'out', // this is the same as the base-path of `:output-to` in `shadow-cljs.edn`
         files: ['test.js'], // this is the same as the file-name (ending with .js) of `:output-to` in `shadow-cljs.edn`
         frameworks: ['cljs-test'],
-        plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+        plugins: ['karma-cljs-test', 'karma-jsdom-launcher'],
         colors: true,
         logLevel: config.LOG_INFO,
         client: {
