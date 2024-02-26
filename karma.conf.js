349576817b9216c81c346fa6e4d42b16467ef294
David Frese <david.frese@active-group.de>
2024-02-26 17:38:56 +0100

    Set host to loopback for karma server.

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (config) {
     config.set({
         browsers: ['jsdom'],
+        hostname: '127.0.0.1',
         basePath: 'out', // this is the same as the base-path of `:output-to` in `shadow-cljs.edn`
         files: ['test.js'], // this is the same as the file-name (ending with .js) of `:output-to` in `shadow-cljs.edn`
         frameworks: ['cljs-test'],
